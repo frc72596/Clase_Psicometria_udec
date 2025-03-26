77da8a007a30af93024ace80bb4e3adebf487383
Your Name <feramirezco@unal.edu.co>
2025-03-26 11:50:56 -0500

    cambios 3

--- Codigo_TCT.R
+++ Codigo_TCT.R
@@ -22,8 +22,21 @@ B2<-correct(dichodata, dichokey, navalue = 0)  # Datos perdidos estan mal
 summary(B1)
 summary(B2)
 # Manejo de los datos perdidos/ Imputación multiple
-
-
+polydata<-as.data.frame(polydata)
+LIKERT<-amelia(polydata,ords = c("item01","item02","item03","item04","item05","item06","item07","item08","item09","item10","item11","item12","item13","item14","item15","item16","item17","item18","item19","item20"))
+B_LIKERT<-LIKERT$imputations[[3]]
 # Analisis de consistenica interna
-reliability(polydata)
-alpha(polydata)
+alpha(polydata) #alfa con datos perdidos -asegurese de usar la función del paquete psych
+alpha(B_LIKERT) #alfa sin datos perdidos- -asegurese de usar la función del paquete psych
+reliability(B_LIKERT) #mas indices de consistencia interna sin datos perdidos-  -asegurese de usar la función del paquete psych
+reliability(dichodata)
+itemAnalysis(B1)
+itemAnalysis(B2)
+dichodata <- na.omit(dichodata)  # Elimina filas con NA
+distractorAnalysis(dichodata,dichokey)
+# rspP- The proportion of respondents with that response
+# pBis- The point-biserial correlation between that reponse and the total score with that item removed
+# discrim- The upper proportion minus the lower proportion
+# lower- The proportion of respondents choosing that response that are from the lowest score group
+# upper- The proportion of respondents choosing that response that are from the highest score group
+
