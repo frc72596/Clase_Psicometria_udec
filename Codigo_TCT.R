17714f779578b1b6ea1cab481ede1f584993b6f3
Your Name <feramirezco@unal.edu.co>
2025-03-26 11:25:37 -0500

    cambios 2

--- Codigo_TCT.R
+++ Codigo_TCT.R
@@ -3,11 +3,13 @@ install.packages("psych")
 install.packages("CTT")
 install.packages("tidyverse")
 install.packages("classicaltest")
+install.packages("Amelia")
 # Llamar a los paquetes
 library(psych)
 library(CTT)
 library(tidyverse)
 library(classicaltest)
+library(Amelia)
 # Base de datos
 data(dichodata)
 data(dichokey)
@@ -19,6 +21,9 @@ B1<-correct(dichodata, dichokey, navalue = NA) # Sigue siendo perdido
 B2<-correct(dichodata, dichokey, navalue = 0)  # Datos perdidos estan mal
 summary(B1)
 summary(B2)
-# 
+# Manejo de los datos perdidos/ Imputación multiple
+
 
 # Analisis de consistenica interna
+reliability(polydata)
+alpha(polydata)
